6f93040e545592aba084a0a48cefa10fea567e5a
itsoumyasanta16 <soumyasantam@gmail.com>
2022-07-02 18:41:48 +0530

    weather_app

---
+++ config.js
@@ -0,0 +1,8 @@
+const constants={
+    openWeatherMap:{
+        BASE_URL:"https://api.openweathermap.org/data/2.5/weather?q=",
+        SECRET_KEY:"89dbb63501978baf6a7f7d5824b8a189"
+    }
+}
+
+module.exports = constants;
